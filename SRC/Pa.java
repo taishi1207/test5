ec138bdec1ca8b86b1cdb4817a67e3b3b393f1e0
taishi <t.komatsu.sys23@morijyobi.ac.jp>
2024-07-19 10:47:13 +0900

    勝敗判定をしたclose #21, close #22

---
+++ SRC/Pa.java
@@ -0,0 +1,45 @@
+public class Pa implements Hand {
+
+   private String HAND_NAME = "パー";
+
+
+
+   /**
+
+    手の強弱を比較する。
+
+    返り値 -1 : 自身が引数で指定した手より弱い
+
+    　　　　0 : 自身が引数で指定した手と同じ
+
+    　　　　1 : 自身が引数で指定した手より強い
+
+    */
+
+   public int compare(Hand hand) {
+
+       if(hand instanceof Gu) {    //チョキ　vs グー（自分）
+
+           return 1;
+
+       } else if(hand instanceof Choki) {    //パー　vs グー（自分）
+
+           return -1;
+
+       } else {
+
+           return 0;    //グー　vs グー（自分）
+
+       }
+
+   }
+
+
+
+   public String getHandName() {
+
+       return this.HAND_NAME;
+
+   }
+
+}
